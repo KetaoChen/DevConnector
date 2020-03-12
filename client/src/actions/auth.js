aa90dc04cef9f5723738bc45c1069da9adf88b66
KetaoChen <kchen343@gatech.edu>
2020-03-12 01:34:37 -0400

    navbar

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,7 +7,8 @@ import {
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  LOGIN_FAIL
+  LOGIN_FAIL,
+  LOGOUT
 } from '../actions/types';
 
 // Load User
@@ -97,4 +98,11 @@ export const login = (email, password) => async dispatch => {
       type: LOGIN_FAIL
     });
   }
+};
+
+// Logout /Clear Profile
+export const logout = () => dispatch => {
+  dispatch({
+    type: LOGOUT
+  });
 };
