a250948763d4e6090744c345b365ee27c5842cef
KetaoChen <kchen343@gatech.edu>
2020-06-22 10:36:48 -0700

    likeupdate and delete post

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,8 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profile from './components/profile/Profile';
 import Profiles from './components/profiles/Profiles';
+import Posts from './components/posts/Posts';
+
 import PrivateRoute from './components/routing/PrivateRoute';
 
 // Redux
@@ -63,6 +65,7 @@ const App = () => {
                 path='/add-education'
                 component={AddEducation}
               />
+              <PrivateRoute exact path='/posts' component={Posts} />
             </Switch>
           </section>
         </Fragment>
